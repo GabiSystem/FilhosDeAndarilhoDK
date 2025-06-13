350940fad71e4583bb8b8004efa1eea319bb97b6
GabiSystem <gabisystemdev@gmail.com>
2025-06-13 10:55:19 -0300

    Menu superior agora funcional em celulares
    mas ainda precisa de alguns ajustes

---
+++ assets/js/menu.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sanduicheBtn = document.getElementById('sanduicheMobile');
+    sanduicheBtn.classList.add('sanduiche-style');
+
+    sanduicheBtn.addEventListener('click', abrirFechar);
+});
+
+function abrirFechar() {
+    const botao = document.getElementById('sanduicheMobile');
+    const menu = document.getElementById('mainNav');
+    const subMenu = document.getElementById('subMenu');
+
+    botao.classList.toggle('aberto');
+    menu.classList.toggle('aberto');
+    subMenu.classList.toggle('aberto');
+    
+}
